44d3e50ab303d3358b8e7ef66b8809227ac90a17
Dima Ivanyuk <dimaivanyuk@gmail.com>
2014-11-27 22:38:57 -0500

    added Passport auth for FB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,20 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+// attempt to build the providers/passport config
+var config = {};
+try {
+	config = require('../providers.json');
+} catch (err) {
+	console.trace(err);
+	process.exit(1); // fatal
+}
+
 // Set up the /favicon.ico
 app.use(loopback.favicon());
 
@@ -17,8 +31,28 @@ boot(app, __dirname);
 // -- Mount static files here--
 // All static middleware should be registered at the end, as all requests
 // passing the static middleware are hitting the file system
-var path = require('path');   
+var path = require('path');
 app.use(loopback.static(path.resolve(__dirname, '../client')));
+
+passportConfigurator.init();
+
+passportConfigurator.setupModels({
+	userModel: app.models.user,
+	userIdentityModel: app.models.userIdentity,
+	userCredentialModel: app.models.userCredential
+});
+
+// passportConfigurator.setupModels();
+for (var s in config) {
+	var c = config[s];
+	c.session = c.session !== false;
+	passportConfigurator.configureProvider(s, c);
+}
+
+/* TODO (for salem) create user profile page */
+app.get('/auth/account', function (req, res, next) {
+
+});
 
 // Requests that get this far won't be handled
 // by any middleware. Convert them into a 404 error
